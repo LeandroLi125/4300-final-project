bab02b6d3b59058e8e9bdf435a3f81cf9b764589
LeandroLi125 <leandro@pdx.edu>
2025-12-10 13:39:52 -0500

    Add files via upload

---
+++ types.py
@@ -0,0 +1,13 @@
+"""Comp Neuro Type Definitions
+
+Defines custom type definitions to help with documentation and sphinx resolution
+"""
+
+import typing as tp
+from scipy.integrate import OdeSolver
+
+# Literal tag for the custom forward Euler solver implemented in BaseModel.solve
+Euler = tp.Literal["Euler"]
+
+# Accept Euler keyword, scipy's odeint string, any other solver name, or an OdeSolver instance
+solvers = tp.Union[Euler, tp.Literal["scipy.integrate.odeint"], str, OdeSolver]
